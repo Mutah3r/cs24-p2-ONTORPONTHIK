f4a094c639c8c449e6c88efe32c1f6528fa21dad
Unayes09 <unayeskhan.0808@gmail.com>
2024-03-26 20:45:23 +0600

    add first time

--- samurai-backend/controllers/auth.js
+++ samurai-backend/controllers/auth.js
@@ -7,10 +7,14 @@ exports.Login = async(req,res)=>{
     const { email, password } = req.body;
     let pass = "";
     let role = "";
+    let isFirstTime = false;
+    let userId="";
     await userModel.findOne({ email:email })
       .then((result) => {
         pass = result.password;
         role = result.role;
+        isFirstTime = result.isLogin
+        userId=result._id;
       })
       .catch((err) => {
         //console.log(pass)
@@ -18,6 +22,7 @@ exports.Login = async(req,res)=>{
     if (pass == "") {
       res.status(401).json({message:"Email is wrong!"});
     } else {
+      await userModel.findByIdAndUpdate(userId, { $set: { isLogin: true } });
       bcrypt.compare(password, pass, function (err, result) {
         if (err) console.log(err);
         if (result) {
@@ -26,7 +31,7 @@ exports.Login = async(req,res)=>{
             process.env.jwt_secret_key,
             { expiresIn: "30d" }
           );
-          res.status(200).json({ token: token,role:role });
+          res.status(200).json({ token: token, role:role, isFirstTime:isFirstTime });
         } else {
           res.status(401).json({message:"Password is wrong!"});
         }
