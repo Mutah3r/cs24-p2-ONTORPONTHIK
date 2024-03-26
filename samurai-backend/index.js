66107111008a4d0a089fc49973906fa5b0c102e5
Mutaher Ahmed Shakil <evil.programmer.stephen@gmail.com>
2024-03-26 15:46:48 +0600

    setup backend

---
+++ samurai-backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+dotenv.config({ path: "./.env" });
+
+mongoose
+  .connect(process.env.URI)
+  .then((err) => {
+    console.log("MyDB is connected");
+  })
+  .catch((err) => {
+    console.log("Check your internet connection");
+  });
+
+
+app.listen(8000, () => {
+  console.log("server is running");
+});
+
+app.use("/auth", require("./routes/auth"));
+app.use("/users", require("./routes/users"));
+app.use("/profile", require("./routes/profile"));
+app.use("/rbac", require("./routes/rbac"));
---
+++ samurai-backend/routes/users.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/users");
+
+//router.post('/teacherRegistration',authController.TeacherRegistration)
+
+module.exports = router;
