b6d8bc6ed5390f65af65cd026b4e9c619d6b31d0
Mutaher Ahmed Shakil <evil.programmer.stephen@gmail.com>
2024-03-26 20:37:00 +0600

    add dashboard demo design

---
+++ samurai-frontend/src/components/DashboardHome/DashboardHome.jsx
@@ -0,0 +1,9 @@
+const DashboardHome = () => {
+  return (
+    <div>
+      <h1>This is dashboard home</h1>
+    </div>
+  );
+};
+
+export default DashboardHome;
--- samurai-frontend/src/routes/Routes.jsx
+++ samurai-frontend/src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
+import Dashboard from "../layouts/Dashboard";
+import DashboardHome from "../components/DashboardHome/DashboardHome";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,16 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "",
+        element: <DashboardHome />,
+      },
+    ],
   },
 ]);
 
