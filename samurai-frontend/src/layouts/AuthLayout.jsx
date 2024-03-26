084887b40b9215a67e5adead414bc254a5e61488
Mutaher Ahmed Shakil <evil.programmer.stephen@gmail.com>
2024-03-26 16:22:56 +0600

    update frontend folder structure

---
+++ samurai-frontend/src/layouts/AuthLayout.jsx
@@ -0,0 +1,9 @@
+const AuthLayout = () => {
+  return (
+    <div>
+      <h1>This is auth layout</h1>
+    </div>
+  );
+};
+
+export default AuthLayout;
---
+++ samurai-frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-amber-500">Welcome to homepage</h1>
+    </div>
+  );
+};
+
+export default Home;
