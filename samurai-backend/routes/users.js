c7f3bff587b77a9ac171f03179d507f63fc175f2
Unayes09 <unayeskhan.0808@gmail.com>
2024-03-26 18:46:09 +0600

    get allusers api

--- samurai-backend/routes/users.js
+++ samurai-backend/routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/users");
 
 router.post('/',authController.Registration)
+router.get('/',authController.getAllUsers)
 
 module.exports = router;
