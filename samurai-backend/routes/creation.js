21abb970106079897ab88775b849cf1b9e23ccfe
Unayes09 <unayeskhan.0808@gmail.com>
2024-03-30 00:51:02 +0600

    manage landfill updated api

--- samurai-backend/routes/creation.js
+++ samurai-backend/routes/creation.js
@@ -6,5 +6,7 @@ router.get('/sts/:token',authController.getSTSInformation)
 router.post('/sts',authController.createSTS)
 router.get('/land/:token',authController.getLandfillInformation)
 router.post('/land',authController.createLandfill)
+router.post('/stsManage',authController.assignManagerToSTS)
+router.post('/landManage',authController.assignManagerToLandfill)
 
 module.exports = router;
