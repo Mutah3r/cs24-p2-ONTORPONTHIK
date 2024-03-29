6ed8e6318c84d626a9f78b207bbcac1a91d43d4a
Mutaher Ahmed Shakil <evil.programmer.stephen@gmail.com>
2024-03-29 15:17:17 +0600

    add facilities management page

---
+++ samurai-frontend/src/pages/FacilityManagement/FacilityManagement.jsx
@@ -0,0 +1,9 @@
+const FacilityManagement = () => {
+  return (
+    <div>
+      <h2>This is facility management</h2>
+    </div>
+  );
+};
+
+export default FacilityManagement;
--- samurai-frontend/src/routes/Routes.jsx
+++ samurai-frontend/src/routes/Routes.jsx
@@ -5,6 +5,7 @@ import Dashboard from "../layouts/Dashboard";
 import DashboardHome from "../components/DashboardHome/DashboardHome";
 import Profile from "../pages/Profile/Profile";
 import UserManagement from "../pages/UserManagement/UserManagement";
+import FacilityManagement from "../pages/FacilityManagement/FacilityManagement";
 
 const router = createBrowserRouter([
   {
@@ -30,6 +31,10 @@ const router = createBrowserRouter([
       {
         path: "users",
         element: <UserManagement />,
+      },
+      {
+        path: "facilities",
+        element: <FacilityManagement />,
       },
     ],
   },
