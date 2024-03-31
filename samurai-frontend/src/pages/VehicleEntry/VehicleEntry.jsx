4f8c5d68c759a370ffdad5613f46bffa1db6982d
Mutaher Ahmed Shakil <evil.programmer.stephen@gmail.com>
2024-03-31 16:11:18 +0600

    design vehicle entry page

--- samurai-frontend/src/pages/VehicleEntry/VehicleEntry.jsx
+++ samurai-frontend/src/pages/VehicleEntry/VehicleEntry.jsx
@@ -5,6 +5,7 @@ import Swal from "sweetalert2";
 const VehicleEntry = () => {
   const [user, setUser] = useState(null);
   const [spinner, setSpinner] = useState(false);
+  const [landfills, setLandfills] = useState([]);
 
   const [formData, setFormData] = useState({
     vehicleRegNo: "",
@@ -13,6 +14,95 @@ const VehicleEntry = () => {
     departureTime: "",
     from: "",
   });
+
+  const [stsFormData, setStsFormData] = useState({
+    vehicleRegNo: "",
+    vehicleType: "",
+    weightCarrying: "",
+    arrivalTime: "",
+    departureTime: "",
+    destination: "",
+  });
+
+  // Function to handle changes in form fields
+  const handleSTSChange = (e) => {
+    const { name, value } = e.target;
+
+    // Validation for Weight Carrying field
+    if (name === "weightCarrying" && !/^\d+([,.]\d+)?$/.test(value)) {
+      return; // Do not update state if input does not match the regex
+    }
+
+    setStsFormData({
+      ...stsFormData,
+      [name]: value,
+    });
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check for empty fields
+    for (const key in stsFormData) {
+      if (stsFormData[key] === "") {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: `Please fill in the ${key
+            .replace(/([A-Z])/g, " $1")
+            .toLowerCase()}.`,
+        });
+        return;
+      }
+    }
+
+    console.log(stsFormData);
+    setSpinner(true);
+    axios
+      .post("http://localhost:8000/sts/entry", {
+        token: JSON.parse(localStorage.getItem("user")),
+        registration_number: stsFormData.vehicleRegNo,
+        type: stsFormData.vehicleType,
+        capacity: parseInt(stsFormData.weightCarrying),
+        time_of_arrival: stsFormData.arrivalTime,
+        time_of_departure: stsFormData.departureTime,
+        to: stsFormData.destination,
+      })
+      .then((res) => {
+        setSpinner(false);
+        console.log(res.data);
+        if (res.data.message === "STSEntry created successfully") {
+          Swal.fire({
+            title: "Good job!",
+            text: "Entry Added!!",
+            icon: "success",
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+          });
+        }
+      })
+      .catch((error) => {
+        setSpinner(false);
+        if (error.response.data.message === "Vehicle not found") {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Vehicle not found!",
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+          });
+        }
+      });
+  };
 
   useEffect(() => {
     axios
@@ -23,6 +113,20 @@ const VehicleEntry = () => {
       )
       .then((res) => {
         setUser(res.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:8000/facilities/allland/${JSON.parse(
+          localStorage.getItem("user")
+        )}`
+      )
+      .then((res) => {
+        const namesArray = res.data.map((item) => item.name);
+        console.log(namesArray);
+        setLandfills(namesArray);
       });
   }, []);
 
@@ -123,7 +227,7 @@ const VehicleEntry = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block mb-1 text-gray-700">Capacity:</label>
+          <label className="block mb-1 text-gray-700">Weight Carrying:</label>
           <input
             type="text"
             name="capacity"
@@ -174,8 +278,102 @@ const VehicleEntry = () => {
         </button>
       </form>
     );
+  } else if (user && user?.role === "STS manager") {
+    return (
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md mx-auto mt-8 p-4 bg-white shadow-md rounded-lg"
+      >
+        <div className="mb-4">
+          <label className="block mb-1 text-gray-700">Vehicle Reg. No.:</label>
+          <input
+            type="text"
+            name="vehicleRegNo"
+            value={stsFormData.vehicleRegNo}
+            onChange={handleSTSChange}
+            className="input input-bordered w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1 text-gray-700">Vehicle Type:</label>
+          <select
+            name="vehicleType"
+            value={stsFormData.vehicleType}
+            onChange={handleSTSChange}
+            className="select select-bordered w-full"
+          >
+            <option value="">Select vehicle type</option>
+            <option value="Open Truck">Open Truck</option>
+            <option value="Dump Truck">Dump Truck</option>
+            <option value="Compactor">Compactor</option>
+            <option value="Container Carrier">Container Carrier</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1 text-gray-700">Weight Carrying:</label>
+          <input
+            type="text"
+            name="weightCarrying"
+            value={stsFormData.weightCarrying}
+            onChange={handleSTSChange}
+            className="input input-bordered w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1 text-gray-700">Time of Arrival:</label>
+          <input
+            type="datetime-local"
+            name="arrivalTime"
+            value={stsFormData.arrivalTime}
+            onChange={handleSTSChange}
+            className="input input-bordered w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1 text-gray-700">Time of Departure:</label>
+          <input
+            type="datetime-local"
+            name="departureTime"
+            value={stsFormData.departureTime}
+            onChange={handleSTSChange}
+            className="input input-bordered w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1 text-gray-700">Destination:</label>
+          <select
+            name="destination"
+            value={stsFormData.destination}
+            onChange={handleSTSChange}
+            className="select select-bordered w-full"
+          >
+            <option value="" disabled>
+              Select landfill
+            </option>
+
+            {landfills &&
+              landfills.map((landfill, idx) => (
+                <option key={idx} value={landfill}>
+                  {landfill}
+                </option>
+              ))}
+          </select>
+        </div>
+        <button
+          disabled={spinner}
+          type="submit"
+          className="btn btn-neutral w-full"
+        >
+          {spinner ? "Loading..." : "Submit"}
+        </button>
+      </form>
+    );
   } else {
-    return <>saldkfj</>;
+    return (
+      <h1 className="text-center text-green -500 font-semibold">
+        STS Logs Entry
+      </h1>
+    );
   }
 };
 
