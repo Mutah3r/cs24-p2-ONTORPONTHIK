cc711e73f26a0207854b25f3e6755f2be0fdcc22
Mutaher Ahmed Shakil <evil.programmer.stephen@gmail.com>
2024-04-01 06:02:25 +0600

    wrap EcoSync logo inside a link tag

--- samurai-frontend/src/components/Logo/Logo.jsx
+++ samurai-frontend/src/components/Logo/Logo.jsx
@@ -1,16 +1,19 @@
 import { IoLeafOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Logo = ({ bgWhite }) => {
   return (
-    <div className="text-green-500 flex gap-2 items-center">
-      <IoLeafOutline className="text-green-500 text-[44px] font-bold" />
-      <h1 className="text-[32px]">
-        Eco
-        <span className={`${bgWhite ? "text-green-500" : "text-white"}`}>
-          Sync
-        </span>
-      </h1>
-    </div>
+    <Link to="/">
+      <div className="text-green-500 flex gap-2 items-center">
+        <IoLeafOutline className="text-green-500 text-[44px] font-bold" />
+        <h1 className="text-[32px]">
+          Eco
+          <span className={`${bgWhite ? "text-green-500" : "text-white"}`}>
+            Sync
+          </span>
+        </h1>
+      </div>
+    </Link>
   );
 };
 
