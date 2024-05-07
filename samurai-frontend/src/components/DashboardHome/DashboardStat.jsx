0020b4882f93db0dc5fb6e0adf6440f2339a89f6
ArfatulAsif <awakicde@gmail.com>
2024-05-07 06:55:23 +0600

    show sts name and landfill name on the assigned manager's dashboard

--- samurai-frontend/src/components/DashboardHome/DashboardStat.jsx
+++ samurai-frontend/src/components/DashboardHome/DashboardStat.jsx
@@ -138,7 +138,7 @@ const DashboardStat = ({ user }) => {
 
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
+      <h1 className="text-2xl font-semibold mb-4">Dashboard {(stsLogs && stsLogs[0])? ` - ${stsLogs[0].sts_name}` : ""} {(landfillLogs && landfillLogs[0])? ` - ${landfillLogs[0].name}` : ""}</h1>
 
       {/* Show loader while fetching data */}
       {loading && (
