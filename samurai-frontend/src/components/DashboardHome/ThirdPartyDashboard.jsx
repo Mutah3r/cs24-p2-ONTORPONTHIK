540339737c8a78e9521245e53b241a8a41bf0ab6
Mutaher Ahmed Shakil <evil.programmer.stephen@gmail.com>
2024-05-10 23:47:25 +0600

    solve merge conflict

--- samurai-frontend/src/components/DashboardHome/ThirdPartyDashboard.jsx
+++ samurai-frontend/src/components/DashboardHome/ThirdPartyDashboard.jsx
@@ -2,49 +2,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const ThirdPartyDashboard = () => {
-<<<<<<< HEAD
-    return (
-        <div className="container mx-auto p-6">
-          <h1 className="text-2xl font-semibold mb-4 text-center">Third-Party Contractor Dashboard</h1> 
-
-          <div className="overflow-x-auto pb-4">
-                <table className="table table-zebra">
-                <thead>
-                    <tr>
-                    <th></th>
-                    <th>Company Name</th>
-                    <th>Reg. ID</th>
-                    <th>Reg. Date</th>
-                    <th>TIN</th>
-                    <th>Contact number</th>
-                    <th>Workforce size</th>
-                    <th>Payment per tonnage of waste</th>
-                    <th>The required amount of waste per day</th>
-                    <th>Contract duration</th>
-                    <th>Area of collection</th>
-                    <th>Designated STS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th></th>
-                        <td>Reg-No:19</td>
-                        <td>5/10/2024</td>
-                        <td>5214789652</td>
-                        <td>0176666666</td>
-                        <td>250</td>
-                        <td>150 Tk</td>
-                        <td>2 Ton</td>
-                        <td>5 years</td>
-                        <td>Dhaka Uttara</td>
-                        <td>10</td>
-                    </tr>
-                </tbody>
-                </table>
-            </div>          
-        </div>
-    );
-=======
   // states
   const [user, setUser] = useState([]);
   const [roles, setRoles] = useState([]);
@@ -78,7 +35,6 @@ const ThirdPartyDashboard = () => {
       <h1 className="text-2xl font-semibold mb-4 text-center">Third-Party Contractor Dashboard</h1>
     </div>
   );
->>>>>>> 0b8909c069a40225b15650241f4049e16243f2cc
 };
 
 export default ThirdPartyDashboard;
