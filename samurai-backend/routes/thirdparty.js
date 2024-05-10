cd08f7792f4db5518854c2678dd3f366471762b4
Arfatul Islam Asif <awakicde@gmail.com>
2024-05-10 23:46:22 +0600

    thirdparty.js

--- samurai-backend/routes/thirdparty.js
+++ samurai-backend/routes/thirdparty.js
@@ -4,8 +4,8 @@ const Controller = require("../controllers/thirdpartyactivities");
 const middleware = require("../middlewars/authentcation")
 
 //get all third party companies
-router.get('/allthirdparties', Controller.getAllThirdPartyContractors)
-//post [create new third party]
-router.post('/all')
+router.get('/allthirdparties', Controller.getAllThirdPartyContractors);
+//post [create new third party] 
+router.post('/thirdparty', Controller.addThirdPartyContractor);
 
 module.exports = router;
