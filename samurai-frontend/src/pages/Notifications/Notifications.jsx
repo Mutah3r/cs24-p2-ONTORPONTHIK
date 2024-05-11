8b57690d8114a7331c4356684831a12d079baf49
Mutaher Ahmed Shakil <evil.programmer.stephen@gmail.com>
2024-05-11 11:16:01 +0600

    add badge for notification cards

--- samurai-frontend/src/pages/Notifications/Notifications.jsx
+++ samurai-frontend/src/pages/Notifications/Notifications.jsx
@@ -7,7 +7,7 @@ const Notifications = () => {
   useEffect(() => {
     axios.get("http://localhost:8000/app/feedback").then((res) => {
       console.log(res.data);
-      setNotifications(res.data);
+      setNotifications(res.data.reverse());
     });
   }, []);
 
@@ -23,6 +23,9 @@ const Notifications = () => {
             return (
               <div key={noti._id} className="card w-full bg-base-100 shadow-xl">
                 <div className="card-body">
+                  <div className="flex justify-end">
+                    <span className="bg-amber-500 rounded-full py-1 px-2 text-[14px] text-white font-semibold">Pending</span>
+                  </div>
                   <div className="grid grid-cols-2 mb-3">
                     <h3><span className="font-semibold">Notification Type:</span> {noti.type}</h3>
                     <h3><span className="font-semibold">User's Email:</span> {noti.user_email}</h3>
